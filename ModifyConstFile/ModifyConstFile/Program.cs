d5148a706c118aa3e6a7f261e38e275a8c1430ef
AtulSShephertz <atul.singh@shephertz.co.in>
2014-01-29 13:13:52 +0530

    Update Program.cs

--- ModifyConstFile/ModifyConstFile/Program.cs
+++ ModifyConstFile/ModifyConstFile/Program.cs
@@ -11,11 +11,11 @@ namespace ModifyConstFile
     {
         static void Main(string[] args)
         {
-            String domainFilePath = @"C:\Automation\AppWarp\src\com\shephertz\app42\server\domain\Constants.java";
+            String domainFilePath = @"C:\Automation\AppWarp\src\com\shephertz\app42\server\AppConstants.java";
             File.WriteAllText(args[0], File.ReadAllText(args[0]).Replace("DB_HOST = \"app42prod.cfdywzjrm34y.us-west-2.rds.amazonaws.com\"", "DB_HOST = \"192.168.1.13\""));
             File.WriteAllText(args[0], File.ReadAllText(args[0]).Replace("DB_PASSWORD = \"App42RootUser\"", "DB_PASSWORD = \"\""));
             File.WriteAllText(args[0], File.ReadAllText(args[0]).Replace("SKIP_LOOKUP = false", "SKIP_LOOKUP = true"));
-            File.WriteAllText(domainFilePath, File.ReadAllText(domainFilePath).Replace("CLEAN_UP_TIME_MILLISECONDS = 3600000", "CLEAN_UP_TIME_MILLISECONDS = 60000"));
+            //File.WriteAllText(domainFilePath, File.ReadAllText(domainFilePath).Replace("CLEAN_UP_TIME_MILLISECONDS = 3600000", "CLEAN_UP_TIME_MILLISECONDS = 60000"));
         }
     }
 }
